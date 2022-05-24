fedf9652f5b44589031eacd126ab689740eca1c0
Phuvalat <phuvalat@icloud.com>
2022-05-24 17:57:28 +0700

    Your Score

---
+++ Exercise4_Phuvalat_V.py
@@ -0,0 +1,9 @@
+FE = 83.9
+GB = 87.9
+ICS = 91.3
+CP = 89.2
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
