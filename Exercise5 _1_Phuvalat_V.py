2b73831f870787cb12399118cd563c8dd4084d68
Phuvalat <phuvalat@icloud.com>
2022-05-30 19:00:06 +0700

    Numeric

---
+++ Exercise5 _1_Phuvalat_V.py
@@ -0,0 +1,12 @@
+num1 = int(input("Number 1 : "))
+num2 = int(input("Number 2 : "))
+
+addition_total = num1+num2
+subtraction_total = num1-num2
+multiplication_total = num1*num2
+division_total = num1/num2
+
+print(num1,"+",num2,"=",addition_total)
+print(num1,"-",num2,"=",subtraction_total)
+print(num1,"*",num2,"=",multiplication_total)
+print(num1,"÷",num2,"=",division_total)
